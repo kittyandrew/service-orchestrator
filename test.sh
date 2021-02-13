62b201f0f4592a3c8dfbba0cde9477bd925d0e8e
kittyandrew <kitty@maudrew.dev>
2021-02-13 16:30:22 +0200

    improve test script

--- test.sh
+++ test.sh
@@ -1,7 +1,13 @@
 
 out=$(curl -s -H "Content-Type: application/json" -H "X-SERVICE: cool_thing" -H "X-TOKEN: secrettoken" http://0.0.0.0:8000)
 echo -e
-echo $out
+if [ -z $out ]; then
+    echo "Could not connect to the server! Start it first."
+    echo -e
+    exit 1
+else
+    echo $out
+fi
 
 out=$(curl -s -X POST -d '{"data": 1}' -H "Content-Type: application/json" -H "X-TOKEN: secrettoken" -H "X-SERVICE: cool_thing" -H "X-URL: https://coolstuff.service.dev" -H "X-TARGET-SERVICE: cool_thing" http://0.0.0.0:8000/subscription/forward)
 echo -e
