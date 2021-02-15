abe455754eb5199d7ab73360e9b53b6483cda275
kittyandrew <kitty@maudrew.dev>
2021-02-15 17:50:10 +0200

    another test

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,8 @@
+use rocket::http::{Status, Header, ContentType};
 use rocket::local::asynchronous::Client;
-use rocket::http::{Status, Header};
+
+
+const TOKEN: &str = "testtoken";
 
 
 #[rocket::async_test]
@@ -27,8 +30,7 @@ async fn test_get_index() {
     }
     // Has good token
     {
-        let token = "testtoken";
-        let resp = client.get("/").header(Header::new("X-TOKEN", token)).dispatch().await;
+        let resp = client.get("/").header(Header::new("X-TOKEN", TOKEN)).dispatch().await;
         assert_eq!(resp.status(), Status::Ok);
 
         let body  = resp.into_string().await.unwrap();
@@ -40,3 +42,42 @@ async fn test_get_index() {
         assert_eq!(body, value);
     }
 }
+
+
+#[rocket::async_test]
+async fn test_bad_auth_on_subscribe() {
+    let client = Client::tracked(super::rocket()).await.unwrap();
+    let url = "/subscription/new";
+    // Missing Token
+    {
+        let resp = client.get(url).dispatch().await;
+        assert_eq!(resp.status(), Status::Unauthorized);
+    }
+    // Missing Headers
+    {
+        let resp = client.get(url)
+            .header(Header::new("X-TOKEN", TOKEN))
+            .dispatch().await;
+        assert_eq!(resp.status(), Status::Unauthorized);
+    }
+    // Missing Headers
+    {
+        let bad_schema = "some bad name";
+        let resp = client.get(url)
+            .header(Header::new("X-TOKEN", TOKEN))
+            .header(Header::new("X-SERVICE", "test_service_1"))
+            .header(Header::new("X-URL", "http://example.com/test_service_1"))
+            .header(Header::new("X-EXPECTED-SCHEMA", bad_schema))
+            .dispatch().await;
+        assert_eq!(resp.status(), Status::Ok);
+
+        let body = resp.into_string().await.unwrap();
+        let value = json!({
+            "msg_code": "err_schema_name_invalid",
+            "message": "Could not map provided schema name to existing schema!",
+            "name": &bad_schema,
+        }).to_string();
+        assert_eq!(body, value);
+    }
+}
+
