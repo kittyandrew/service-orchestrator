d7f66f8a08dd03153fb4431d4cf912a813bba970
kittyandrew <kitty@maudrew.dev>
2021-02-16 16:12:35 +0200

    add tests

--- src/tests.rs
+++ src/tests.rs
@@ -179,5 +179,71 @@ async fn test_bad_token_subscription_forward() {
 }
 
 
+#[rocket::async_test]
+async fn invalid_schema_subscription_forward() {
+    let client = Client::tracked(super::rocket()).await.unwrap();
+    let nurl = "/subscription/new";
+    let furl = "/subscription/forward";
+    let token: String;
+    // Registering client with schema 1
+    {
+        let schema = "schema";
+        let resp = client.get(nurl)
+            .header(Header::new("X-TOKEN", TOKEN))
+            .header(Header::new("X-SERVICE", "test_service_1"))
+            .header(Header::new("X-URL", "http://example.com/test_service_1"))
+            .header(Header::new("X-EXPECTED-SCHEMA", schema))
+            .dispatch().await;
+        assert_eq!(resp.status(), Status::Ok);
+
+        let json: JsonValue = from_str(&resp.into_string().await.unwrap()).unwrap();
+        assert_eq!(json["msg_code"], String::from("info_subscription_ok"));
+        assert_eq!(json["message"], String::from("Successfully subscribed to the orchestrator!"));
+        // Verify token is something
+        assert!(json["new_token"].is_string());
+        let t = json["new_token"].as_str().unwrap().to_string();
+        assert!(!t.is_empty());
+        // Save to compare later
+        token = t;
+    }
+    // Testing body
+    {
+        let resp = client.post(furl)
+            .body("\"This is a json string!\"")
+            .header(ContentType::JSON)
+            .header(Header::new("X-TOKEN", token.clone()))
+            .header(Header::new("X-SERVICE", "test_service_1"))
+            .header(Header::new("X-TARGET-SERVICE", "test_service_1"))
+            .dispatch().await;
+        assert_eq!(resp.status(), Status::Ok);
+
+        let body = resp.into_string().await.unwrap();
+        let value = json!({
+            "msg_code": "err_schema_invalid",
+            "message": "Validation error: '\"This is a json string!\"' is longer than 5 characters",
+        }).to_string();
+        assert_eq!(body, value);
+    }
+    // Testing body
+    {
+        let resp = client.post(furl)
+            .body("{\"key\": \"value\"}")
+            .header(ContentType::JSON)
+            .header(Header::new("X-TOKEN", token.clone()))
+            .header(Header::new("X-SERVICE", "test_service_1"))
+            .header(Header::new("X-TARGET-SERVICE", "test_service_1"))
+            .dispatch().await;
+        assert_eq!(resp.status(), Status::Ok);
+
+        let body = resp.into_string().await.unwrap();
+        let value = json!({
+            "msg_code": "err_schema_invalid",
+            "message": "Validation error: '{\"key\":\"value\"}' is not of type 'string'",
+        }).to_string();
+        assert_eq!(body, value);
+    }
+}
+
+
 // TODO: good auth / good forward
 
